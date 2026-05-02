665a740c301e919d543123f0d807329555ec112d
Marisa Muce <mars@indrastructible.net>
2026-05-02 22:43:15 +0000

    Update 50-upgrade.sh

--- 10-portainer/50-upgrade.sh
+++ 10-portainer/50-upgrade.sh
@@ -10,4 +10,4 @@ docker rm portainer
 docker pull portainer/portainer-ce:lts
 
 # Deploy the updated version of Portainer
-docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+docker run -d -p 127.0.0.1:9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
